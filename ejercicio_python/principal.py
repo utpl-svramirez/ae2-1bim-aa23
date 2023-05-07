e0fdc698c10ac41dce5c2bda41c9d2bbfcc5462b
utpl-svramirez <svramirez@utpl.edu.ec>
2023-05-06 19:58:39 -0500

    modificacion style

--- ejercicio_python/principal.py
+++ ejercicio_python/principal.py
@@ -3,13 +3,14 @@ from python_svramirez import Universidad
 contador = 1
 
 while(contador<=3):
-    nombre_universidad = input("Ingrese nombre de universidad: ")
-    num_profesores = input("Ingrese número de profesores: ")
+    nombre_universidad = input("Ingrese nombre de universidad : ")
+    num_profesores = input("Ingrese número de profesores : ")
     num_profesores = int(num_profesores)
-    num_alumnos = input("Ingrese número de alumnos: ")
+    num_alumnos = input("Ingrese número de alumnos : ")
     num_alumnos = int(num_alumnos)
     # se crea el objeto
     u = Universidad(nombre_universidad, num_profesores, num_alumnos)
     print(u)
     # se incrementa el contador
     contador = contador + 1
+    
