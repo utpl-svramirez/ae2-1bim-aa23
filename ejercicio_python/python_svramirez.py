f837ba5b6ef0e10f17510350ce87e332cde64f59
utpl-svramirez <svramirez@utpl.edu.ec>
2023-05-06 19:53:20 -0500

    primera carga

---
+++ ejercicio_python/python_svramirez.py
@@ -0,0 +1,28 @@
+class Universidad:
+    def __init__(self, name, n_carreras, n_facultades):
+        self.nombre = name
+        self.profesores = n_carreras
+        self.alumnos = n_facultades
+
+    def establecer_nombre(self, n):
+         self.nombre = n
+
+    def establecer_carreras(self, n):
+        self.carreras = n
+
+    def establecer_facultades(self, n):
+        self.facultades = n
+
+    def obtener_nombre(self):
+        return self.nombre
+
+    def obtener_carreras(self):
+        return self.carreras
+    
+    def obtener_facultades(self):
+         return self.facultades
+    
+    def __str__(self):
+        return "Universidad: %s, Número de carreras: %d, Número de facultades: %d\n" \
+            %( self.nombre, self.pcarreras, self.afacultades)
+                
