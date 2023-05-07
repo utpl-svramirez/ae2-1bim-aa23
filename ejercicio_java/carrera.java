25dea0055838bb90e046f49a025b00f2db5746bb
utpl-svramirez <svramirez@utpl.edu.ec>
2023-05-06 20:03:15 -0500

    carga archivo java

---
+++ ejercicio_java/carrera.java
@@ -0,0 +1,30 @@
+public class carrera{
+   
+    private String nombreFacultad;
+    private String nombreCarrera;
+    private int cantidadSemestres;  
+
+    public String getNombreFacultad() {
+        return nombreFacultad;
+    }
+
+    public void setNombreFacultad(String nombreFacultad) {
+        this.nombreFacultad = nombreFacultad;
+    }
+
+    public String getNombreCarrera() {
+        return nombreCarrera;
+    }
+
+    public void setNombreCarrera(String nombreCarrera) {
+        this.nombreCarrera = nombreCarrera;
+    }
+
+    public int getCantidadSemestres() {
+        return cantidadSemestres;
+    }
+
+    public void setCantidadSemestres(int cantidadSemestres) {
+        this.cantidadSemestres = cantidadSemestres;
+    }
+}
